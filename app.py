22dc9e81ff5fe3ca1429d293b6e0456f4b59a272
Ossama <osamaalyafay2@gmail.com>
2026-02-20 06:45:19 +0300

    Fix  strtup app,py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import UPLOAD_FOLDER
+import os
 
 from routes.admin_users import admin_users_bp
 from routes.colleges import colleges_bp
@@ -9,9 +10,6 @@ from routes.levels import levels_bp
 from routes.subjects import subjects_bp
 from routes.contents import contents_bp
 from routes.auth import auth_bp
-
-import threading
-import os
 
 
 # ==========================================
@@ -38,28 +36,8 @@ app.register_blueprint(admin_users_bp)
 
 
 # ==========================================
-# تشغيل البوت داخل Render
-# ==========================================
-
-def start_telegram_bot():
-    print("Starting Telegram bot thread...")
-    from bot import start_bot
-    start_bot()
-
-
-# مهم: هذا الكود يُنفذ عند استيراد الملف بواسطة Gunicorn
-if os.environ.get("RENDER") == "1":
-    print("Render environment detected. Launching bot...")
-    threading.Thread(
-        target=start_telegram_bot,
-        daemon=True
-    ).start()
-
-
-# ==========================================
 # تشغيل محلي فقط
 # ==========================================
 
 if __name__ == "__main__":
-    print("Running locally...")
     app.run(debug=True)
